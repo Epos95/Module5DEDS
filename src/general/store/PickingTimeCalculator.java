7706352b52a8fd32f6334b3a9c48f7e25385c721
toastyengineer <43987577+toastyengineer@users.noreply.github.com>
2021-03-10 18:34:24 +0100

    småfix
    Behöver vi använda båda metoderna i Håkans "random" klasser? idk

--- src/general/store/PickingTimeCalculator.java
+++ src/general/store/PickingTimeCalculator.java
@@ -3,11 +3,8 @@ package general.store;
 public class PickingTimeCalculator {
     UniformRandomStream random;
 
-    public PickingTimeCalculator(float min, float max, int seed) {
-        random = new UniformRandomStream(min, max, seed);
-    }
-
-    public PickingTimeCalculator(double pickingMin, double pickingMax, int randomizerSeed) {
+    public PickingTimeCalculator(double pickingMin, double pickingMax, int seed) {
+        random = new UniformRandomStream(pickingMin, pickingMax, seed);
     }
 
     public double getTime() {
