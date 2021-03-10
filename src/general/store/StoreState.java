6340a8955b573a77bc2374fbdf50ab236ff45a4b
epos95 <maxagnesund95@gmail.com>
2021-03-10 14:45:12 +0100

    Last time I said it works? I was kidding.  Try this.

--- src/general/store/StoreState.java
+++ src/general/store/StoreState.java
@@ -2,17 +2,19 @@ package general.store;
 
 import general.State;
 
-public class StoreState extends State {
+import java.util.Observable;
+
+public class StoreState extends Observable {
     int cashRegisters;
-    int maxCustomers;
+    protected int maxCustomers;
     // changed to double from int due to time being a double
-    double arriveInterval;
+    protected double arriveInterval;
 
     // changed from the uml doc
-    double currentTime;
+    protected double currentTime;
 
     // these are not specified in uml but are needed to operate the events
     public boolean isOpen;
-    int currentCustomers;
+    protected int currentCustomers;
 
 }
