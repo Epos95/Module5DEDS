64e07c34c98b659aef37d4742c3a8dce7ad81197
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:29:39 +0100

    Update EventQueue.java

--- src/lab5/general/EventQueue.java
+++ src/lab5/general/EventQueue.java
@@ -2,21 +2,29 @@ package lab5.general;
 
 import java.util.ArrayList;
 
+/**
+*<p>
+*A queue with all the events about to happen where the event
+*that's next to occur is in front of the queue.
+*</p>
+*@author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+*Max Agnesund
+*/
 public class EventQueue {
 	
 	private ArrayList<Event> queue = new ArrayList<Event>();
 
 	/**
-	 * 
-	 * @return
+	 * Returns the length of the event queue.
+	 * @return Length of the event queue.
 	 */
 	public int len() {
 		return queue.size();
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Removes the next event to be executed from queue.
+	 * @return The removed event from the queue.
 	 */
 	public Event pop() {
 		Event e = queue.get(0);
@@ -25,8 +33,8 @@ public class EventQueue {
 	}
 
 	/**
-	 * 
-	 * @param e
+	 * Adds an event to the event queue.
+	 * @param e An event.
 	 */
 	public void push(Event e) {
 		addToQueue(e);
@@ -34,8 +42,8 @@ public class EventQueue {
 
 
 	/**
-	 * 
-	 * @param e
+	 * Adds an event to the queue and in the order in which it is to be exectued.
+	 * @param e Event to be added to the event queue.
 	 */
 	public void addToQueue(Event e) {
 		if (queue.size() == 0) {
