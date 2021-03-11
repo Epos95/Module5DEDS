d4fe3afca4cd88878f021fa2e3a3cdc30d739ed2
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:46:17 +0100

    Update EndEvent.java

--- src/lab5/general/store/events/EndEvent.java
+++ src/lab5/general/store/events/EndEvent.java
@@ -4,15 +4,22 @@ import lab5.general.Event;
 import lab5.general.EventQueue;
 import lab5.general.store.StoreState;
 
+/**
+ * <p>
+ * Event that stops the simulation.
+ * </p>
+ * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+ * Max Agnesund
+ */
 public class EndEvent extends Event {
     StoreState state;
     EventQueue queue;
 
     /**
-     * 
-     * @param q
-     * @param s
-     * @param o
+     * Constructor for EndEvent.
+     * @param q The event queue.
+     * @param s The store state.
+     * @param o Time of end.
      */
     public EndEvent(EventQueue q, StoreState s, double o) {
         this.occurenceTime = o;
@@ -21,7 +28,7 @@ public class EndEvent extends Event {
     }
 
     /**
-     * 
+     * Method to make the method occur. Stops the simulation.
      */
     @Override
     public void execute() {
