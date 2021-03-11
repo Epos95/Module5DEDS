6fab47349a2e543f14b195c55e43f0ad7384d23c
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:38:18 +0100

    Update Customer.java

--- src/lab5/general/store/Customer.java
+++ src/lab5/general/store/Customer.java
@@ -1,18 +1,24 @@
 package lab5.general.store;
 
+/**
+ * <p>
+ * Creates a customer with an id.
+ * </p>
+ * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+ * Max Agnesund
+ */
 public class Customer {
     private final int id;
 
     /**
-     * 
-     * @param newId
+     * @param newId the id of the new customer.
      */
     public Customer(int newId) {
         this.id = newId;
     }
 
     /**
-     * 
+     * @return The id as a string.
      */
     public String toString() {
         return Integer.toString(id);
