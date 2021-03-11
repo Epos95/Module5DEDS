06f25099bce1667564af110e3405d91e2f3faef4
epos95 <maxagnesund95@gmail.com>
2021-03-11 16:30:05 +0100

    yes

--- src/lab5/general/store/events/PaidEvent.java
+++ src/lab5/general/store/events/PaidEvent.java
@@ -9,63 +9,11 @@ import lab5.general.store.StoreState;
  * <p>
  * Event for a customer paying at the checkout.
  * </p>
- * 
- * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and Max
- *         Agnesund
+ * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and
+ * Max Agnesund
  */
 public class PaidEvent extends Event {
 
-<<<<<<< HEAD
-	Customer customer;
-	StoreState state;
-
-	/**
-	 * Constructor for paidEvent.
-	 * 
-	 * @param q The event queue.
-	 * @param s The state of the store.
-	 * @param o Time of event happening.
-	 * @param c Customer performing event.
-	 */
-	public PaidEvent(EventQueue q, StoreState s, double o, Customer c) {
-		// generate time and stuff
-		this.occurenceTime = o;
-		this.queue = q;
-		this.customer = c;
-		this.state = s;
-	}
-
-	/**
-	 * Method that makes the paying event happen.
-	 */
-	@Override
-	public void execute() {
-		state.updateTime(occurenceTime);
-		state.sendUpdate("Betalning", customer.toString());
-		state.paidCustomer();
-		state.currentCustomers -= 1;
-
-		//
-		if (state.cQueue.isEmpty()) {
-
-			// no one is in queue, register opens up
-			state.openRegister();
-
-			//
-			if (!state.isOpen && state.currentCustomers == 0) {
-				state.closeStore();
-				this.queue.push(new EndEvent(this.queue, this.state, 999));
-			}
-
-		} else {
-
-			//
-			this.queue.push(new PaidEvent(this.queue, state,
-					state.cashierSpeed.getTime() + state.currentTime,
-					state.cQueue.dequeue()));
-		}
-	}
-=======
     Customer customer;
     StoreState state;
 
@@ -89,28 +37,27 @@ public class PaidEvent extends Event {
      */
     @Override
     public void execute() {
-    	state.updateTime(occurenceTime);
+        state.updateTime(occurenceTime);
         state.sendUpdate("Betalning", customer.toString());
         state.currentCustomers -= 1;
         //
         if (state.cQueue.isEmpty()) {
-        	
-        	// no one is in queue, register opens up
-        	state.openRegister();
-        	
-        	//
-        	if (!state.isOpen && state.currentCustomers == 0) {
-        		state.closeStore();
-        		this.queue.push(new EndEvent(this.queue, this.state, 999));
-        	}
+
+            // no one is in queue, register opens up
+            state.openRegister();
+
+            //
+            if (!state.isOpen && state.currentCustomers == 0) {
+                state.closeStore();
+                this.queue.push(new EndEvent(this.queue, this.state, 999));
+            }
 
         } else {
-        	
-        	//
+
+            //
             this.queue.push(new PaidEvent(this.queue, state, state.cashierSpeed.getTime()+state.currentTime, state.cQueue.dequeue()));
         }
 
         state.paidCustomer();
     }
->>>>>>> 4c26ec2b73234ceacb6a0bfc269d34a314bd7b37
 }
--- src/lab5/general/store/events/ReadyToPayEvent.java
+++ src/lab5/general/store/events/ReadyToPayEvent.java
@@ -9,54 +9,50 @@ import lab5.general.store.StoreState;
  * <p>
  * Event For a customer that has picked his wares and is ready to pay.
  * </p>
- * 
- * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and Max
- *         Agnesund
+ * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and
+ * Max Agnesund
  */
 public class ReadyToPayEvent extends Event {
 
-	Customer customer;
-	StoreState state;
+    Customer customer;
+    StoreState state;
 
-	/**
-	 * Constructor for readyToPayEvent.
-	 * 
-	 * @param q    The event queue.
-	 * @param s    The store state.
-	 * @param time Time of event occurring.
-	 * @param c    THe customer that perform this event.
-	 */
-	public ReadyToPayEvent(EventQueue q, StoreState s, double time,
-			Customer c) {
-		// generate time here somehow
-		this.occurenceTime = time;
-		this.queue = q;
-		this.customer = c;
-		this.state = s;
-	}
+    /**
+     * Constructor for readyToPayEvent.
+     * @param q The event queue.
+     * @param s The store state.
+     * @param time Time of event occuring.
+     * @param c THe customer that performe this event.
+     */
+    public ReadyToPayEvent(EventQueue q, StoreState s, double time, Customer c) {
+        // generate time here somehow
+        this.occurenceTime = time;
+        this.queue = q;
+        this.customer = c;
+        this.state = s;
+    }
 
-	/**
-	 * <p>
-	 * Puts the customer in the cash register queue if there's none available.
-	 * Otherwise allows customer to pay.
-	 * </p>
-	 */
-	@Override
-	public void execute() {
-		state.updateTime(occurenceTime);
+    /**
+     * <p>
+     * Puts the customer in the cash register queue if there's none vailable.
+     * Otherwise allows customer to pay.
+     * </p>
+     */
+    @Override
+    public void execute() {
+        state.updateTime(occurenceTime);
 
-		state.sendUpdate("Plock", customer.toString());
-		// no open registers
-		// add customer to queue of customers
-		if (state.freeCashRegisters == 0) {
-			state.cQueue.enqueue(this.customer);
-			state.queuedCustomer();
-		} else {
-			state.closeRegister();
-			this.queue.addToQueue(
-					new PaidEvent(queue, state, state.currentTime +
-							state.cashierSpeed.getTime(), customer));
-		}
+        state.sendUpdate("Plock", customer.toString());
+        // no open registers
+        // add customer to queue of customers
+        if (state.freeCashRegisters == 0) {
+            state.cQueue.enqueue(this.customer);
+            state.queuedCustomer();
+        } else {
+            state.closeRegister();
+            this.queue.addToQueue(
+                    new PaidEvent(queue, state, state.currentTime + state.cashierSpeed.getTime(), customer));
+        }
 
-	}
+    }
 }
