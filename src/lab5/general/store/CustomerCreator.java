6a8d885da7a52b76a444a6e85160b2657028334f
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:38:42 +0100

    Update CustomerCreator.java

--- src/lab5/general/store/CustomerCreator.java
+++ src/lab5/general/store/CustomerCreator.java
@@ -1,11 +1,17 @@
 package lab5.general.store;
 
+/**
+ * <p>
+ * Creates a customers id.
+ * </p>
+ * @author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+ * Max Agnesund
+ */
 public class CustomerCreator {
     private int idIterator = 0;
 
     /**
-     * 
-     * @return
+     * @return A new customer.
      */
     public Customer getCustomer() {
         Customer customer = new Customer(idIterator);
