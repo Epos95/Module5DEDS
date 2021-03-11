c5d034f4645838161347ac8a11709f5276070e0d
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:28:22 +0100

    Update Event.java

--- src/lab5/general/Event.java
+++ src/lab5/general/Event.java
@@ -1,5 +1,13 @@
 package lab5.general;
 
+/**
+*<p>
+*Extended by all specific events.
+*Has an execute method to make the event happen.
+*</p>
+*@author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+*Max Agnesund.
+ */
 abstract public class Event {
 	protected EventQueue queue;
 	protected double occurenceTime;
