996a958fc9a5d383184d4cd0775e25523944a299
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:33:17 +0100

    Update GeneralStopEvent.java

--- src/lab5/general/GeneralStopEvent.java
+++ src/lab5/general/GeneralStopEvent.java
@@ -1,15 +1,32 @@
 package lab5.general;
 
+/**
+ * <p>
+ * Event to stop the simulation.
+ * </p>
+ *@author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+*Max Agnesund.
+ */
 public class GeneralStopEvent extends Event {
     private double time;
     private State state;
-
+/**
+* <p>
+* Constructor for GeneralStopEvent.
+* </p>
+* @param t Time of event occurence.
+* @param state The state.
+*/
     public GeneralStopEvent(double t, State state) {
         this.time = t;
         this.state = state;
     }
 
-    @Override
+/**
+* <p>
+* Stops the simulation.
+* </p>
+*/
     public void execute() {
         state.currentTime = this.time;
         state.notifyObservers();
