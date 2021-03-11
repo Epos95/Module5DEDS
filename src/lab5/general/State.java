f658d25237d5547e60bc3e63360249f8003995fc
Elliotjf <80103646+Elliotjf@users.noreply.github.com>
2021-03-11 14:34:23 +0100

    Update State.java

--- src/lab5/general/State.java
+++ src/lab5/general/State.java
@@ -2,6 +2,14 @@ package lab5.general;
 
 import java.util.Observable;
 
+/**
+*<p>
+*The base state. Extended by simulation specific states.
+*Keeps track of the time variable and "if the program is running" variable. 
+*</p>
+*@author Anton Lundmark, Elliot Johansson Fryklöf, Karolina Rucinska and 
+*Max Agnesund
+*/
 @SuppressWarnings("deprecation")
 public class State extends Observable{
 	
